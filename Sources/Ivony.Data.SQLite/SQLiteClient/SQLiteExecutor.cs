966e112b2ab327504b0646fb964edce405cf38be
Ivony <Ivony@live.com>
2014-06-13 09:35:35 +0800

    新增 SQLite 支持

---
+++ Sources/Ivony.Data.SQLite/SQLiteClient/SQLiteExecutor.cs
@@ -0,0 +1,28 @@
+﻿using Ivony.Data.Common;
+using Ivony.Data.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ivony.Data.SQLiteClient
+{
+  public class SQLiteExecutor : DbExecutorBase, IDbExecutor<ParameterizedQuery>
+  {
+
+    public SQLiteExecutor( string connectionString, SQLiteConfiguration configuration )
+      : base( configuration )
+    {
+      ConnectionString = connectionString;
+    }
+
+    protected string ConnectionString
+    {
+      get;
+      private set;
+    }
+
+
+  }
+}
