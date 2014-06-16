eb707c6f5845388f4e357066b6168e17456ba49b
Ivony <Ivony@live.com>
2014-06-16 17:44:21 +0800

    新增 SQLite 支持

---
+++ Sources/Ivony.Data.SQLite/SQLiteClient/SQLiteExecuteContext.cs
@@ -0,0 +1,25 @@
+﻿using Ivony.Data.Common;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+
+namespace Ivony.Data.SQLiteClient
+{
+  class SQLiteExecuteContext : AsyncDbExecuteContextBase
+  {
+    public SQLiteExecuteContext( SQLiteConnection connection, SQLiteDataReader dataReader, IDbTracing tracing )
+      : base( dataReader, connection, tracing )
+    {
+      DataReader = dataReader;
+    }
+
+
+    public new SQLiteDataReader DataReader
+    {
+      get;
+      private set;
+    }
+  }
+}
--- Sources/Ivony.Data.SQLite/SQLiteClient/SQLiteExecutor.cs
+++ Sources/Ivony.Data.SQLite/SQLiteClient/SQLiteExecutor.cs
@@ -2,6 +2,8 @@
 using Ivony.Data.Queries;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,7 +29,33 @@ namespace Ivony.Data.SQLiteClient
 
     public IDbExecuteContext Execute( ParameterizedQuery query )
     {
-      throw new NotImplementedException();
+      var command = new SQLiteParameterizedQueryParser().Parse( query );
+
+      return Execute( command, TryCreateTracing( this, query ) );
+    }
+
+    private IDbExecuteContext Execute( SQLiteCommand command, IDbTracing tracing )
+    {
+      try
+      {
+        TryExecuteTracing( tracing, t => t.OnExecuting( command ) );
+
+        var connection = new SQLiteConnection( ConnectionString );
+        connection.Open();
+        command.Connection = connection;
+
+        var context = new SQLiteExecuteContext( connection, command.ExecuteReader(), tracing );
+
+        TryExecuteTracing( tracing, t => t.OnLoadingData( context ) );
+
+        return context;
+      }
+      catch( DbException exception )
+      {
+        TryExecuteTracing( tracing, t => t.OnException( exception ) );
+        throw;
+      }
+
     }
   }
 }
