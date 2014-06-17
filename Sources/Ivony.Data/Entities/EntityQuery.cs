c0aa43e3906c8e94d75fb078f55a2ae03f254524
Ivony <Ivony@live.com>
2014-06-17 15:04:19 +0800

    新增 EntityQuery

---
+++ Sources/Ivony.Data/Entities/EntityQuery.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ivony.Data.Entities
+{
+  public abstract class EntityQuery<T> : IDbQuery
+  {
+
+    protected PropertyInfo[] GetKeyProperties()
+    {
+      throw new NotImplementedException();
+    }
+
+    
+
+  }
+}
---
+++ Sources/Ivony.Data/Entities/UpdateEntityQuery.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ivony.Data.Entities
+{
+  public class UpdateEntityQuery<T> : EntityQuery<T>
+  {
+
+    public UpdateEntityQuery( T entity )
+    {
+      Entity = entity;
+    }
+
+
+
+
+    public T Entity { get; private set; }
+
+  }
+}
