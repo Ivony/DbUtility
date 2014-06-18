61a245d9292bf1f97b32737612a714d8efe990f7
Ivony <Ivony@live.com>
2014-06-18 17:16:42 +0800

    SqlDbExecutorWithTransaction 类型移动到 SqlDbTransactionContext 类型内部

--- Sources/Ivony.Data/SqlClient/SqlDbTransactionContext.cs
+++ Sources/Ivony.Data/SqlClient/SqlDbTransactionContext.cs
@@ -2,9 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.Common;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Ivony.Data.SqlClient
@@ -59,5 +61,98 @@ namespace Ivony.Data.SqlClient
     {
       get { return _executor; }
     }
+
+
+
+    private class SqlDbExecutorWithTransaction : SqlDbExecutor
+    {
+      public SqlDbExecutorWithTransaction( SqlDbTransactionContext transaction, SqlDbConfiguration configuration )
+        : base( transaction.Connection.ConnectionString, configuration )
+      {
+        TransactionContext = transaction;
+      }
+
+
+      /// <summary>
+      /// 当前所处的事务
+      /// </summary>
+      protected SqlDbTransactionContext TransactionContext
+      {
+        get;
+        private set;
+      }
+
+
+      /// <summary>
+      /// 重写 ExecuteAsync 方法，在事务中异步执行查询
+      /// </summary>
+      /// <param name="command">要执行的查询命令</param>
+      /// <param name="token">取消指示</param>
+      /// <param name="tracing">用于追踪的追踪器</param>
+      /// <returns>查询执行上下文</returns>
+      protected sealed override async Task<IAsyncDbExecuteContext> ExecuteAsync( SqlCommand command, CancellationToken token, IDbTracing tracing = null )
+      {
+        try
+        {
+          TryExecuteTracing( tracing, t => t.OnExecuting( command ) );
+
+          command.Connection = TransactionContext.Connection;
+          command.Transaction = TransactionContext.Transaction;
+
+          if ( Configuration.QueryExecutingTimeout.HasValue )
+            command.CommandTimeout = (int) Configuration.QueryExecutingTimeout.Value.TotalSeconds;
+
+
+          var reader = await command.ExecuteReaderAsync( token );
+          var context = new SqlDbExecuteContext( TransactionContext, reader, tracing );
+
+          TryExecuteTracing( tracing, t => t.OnLoadingData( context ) );
+
+          return context;
+        }
+        catch ( DbException exception )
+        {
+          TryExecuteTracing( tracing, t => t.OnException( exception ) );
+          throw;
+        }
+
+      }
+
+
+      /// <summary>
+      /// 执行查询命令并返回执行上下文
+      /// </summary>
+      /// <param name="command">查询命令</param>
+      /// <param name="tracing">用于追踪查询过程的追踪器</param>
+      /// <returns>查询执行上下文</returns>
+      protected sealed override IDbExecuteContext Execute( SqlCommand command, IDbTracing tracing = null )
+      {
+        try
+        {
+          TryExecuteTracing( tracing, t => t.OnExecuting( command ) );
+
+          command.Connection = TransactionContext.Connection;
+          command.Transaction = TransactionContext.Transaction;
+
+          if ( Configuration.QueryExecutingTimeout.HasValue )
+            command.CommandTimeout = (int) Configuration.QueryExecutingTimeout.Value.TotalSeconds;
+
+
+          var reader = command.ExecuteReader();
+          var context = new SqlDbExecuteContext( TransactionContext, reader, tracing );
+
+          TryExecuteTracing( tracing, t => t.OnLoadingData( context ) );
+
+          return context;
+        }
+        catch ( DbException exception )
+        {
+          TryExecuteTracing( tracing, t => t.OnException( exception ) );
+          throw;
+        }
+      }
+
+    }
+
   }
 }
