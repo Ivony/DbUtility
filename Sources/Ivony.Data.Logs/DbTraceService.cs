9be05b067a5271395b8ee6d16d0718a0f282f90b
Ivony <Ivony@live.com>
2014-06-19 18:55:20 +0800

    新增 Ivony.Data.Logs 项目

---
+++ Sources/Ivony.Data.Logs/DbTraceService.cs
@@ -0,0 +1,96 @@
+﻿using Ivony.Logs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ivony.Data.Logs
+{
+  public class DbTraceService : IDbTraceService
+  {
+
+    public DbTraceService( Logger logger )
+    {
+
+      Logger = logger;
+
+    }
+
+    protected Logger Logger
+    {
+      get;
+      private set;
+    }
+
+
+
+
+
+    public IDbTracing CreateTracing<TQuery>( IDbExecutor<TQuery> executor, TQuery query ) where TQuery : IDbQuery
+    {
+      return new DbTracing( query, LogTracing );
+    }
+
+    protected virtual void LogTracing( DbTracing tracing )
+    {
+      Logger.LogEntry( CreateEntry( tracing ) );
+    }
+
+    protected LogEntry CreateEntry( DbTracing tracing )
+    {
+      if ( tracing.Exception != null )
+      {
+
+        using ( var writer = new StringWriter() )
+        {
+
+          writer.WriteLine( "执行数据查询时出现异常" );
+          writer.WriteLine( "执行的查询：" );
+          writer.WriteLine( tracing.QueryObject );
+          writer.WriteLine( new string( '-', 30 ) );
+          if ( tracing.CommandObject != null )
+          {
+            writer.WriteLine( "命令对象：" );
+            writer.WriteLine( tracing.CommandObject );
+            writer.WriteLine( new string( '-', 30 ) );
+          }
+
+          writer.WriteLine( "查询花费时间： {0}", tracing.QueryTime );
+          writer.WriteLine( "异常详细信息：" );
+          writer.WriteLine( tracing.Exception );
+          writer.WriteLine( new string( '=', 30 ) );
+
+          return new LogEntry( writer.ToString(), new LogMeta() { Type = LogType.Exception } );
+        }
+      }
+      else
+      {
+        using ( var writer = new StringWriter() )
+        {
+
+          writer.WriteLine( "成功执行查询" );
+
+          writer.WriteLine( "执行的查询：" );
+          writer.WriteLine( tracing.QueryObject );
+          writer.WriteLine( new string( '-', 30 ) );
+
+          writer.WriteLine( "命令对象：" );
+          writer.WriteLine( tracing.CommandObject );
+          writer.WriteLine( new string( '-', 30 ) );
+
+
+          writer.WriteLine( "数据库执行时间： {0}", tracing.ExecutionTime );
+          writer.WriteLine( "查询总计花费： {0}", tracing.QueryTime );
+          writer.WriteLine( new string( '=', 30 ) );
+
+          return new LogEntry( writer.ToString(), new LogMeta() { Type = LogType.Info } );
+        }
+      }
+    }
+
+
+
+  }
+}
---
+++ Sources/Ivony.Data/DbTracing.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ivony.Data
+{
+  public class DbTracing : IDbTracing
+  {
+
+
+    public DbTracing( IDbQuery query ) : this( query, null ) { }
+
+
+    public DbTracing( IDbQuery query, Action<DbTracing> complateCallback )
+    {
+
+      QueryObject = query;
+
+      ExecutionTime = TimeSpan.Zero;
+      QueryTime = TimeSpan.Zero;
+
+
+      callback = complateCallback;
+    }
+
+
+
+
+    private Stopwatch executionStopwatch = new Stopwatch();
+    private Stopwatch queryStopwatch = new Stopwatch();
+
+
+
+    public IDbQuery QueryObject
+    {
+      get;
+      private set;
+    }
+
+    public object CommandObject
+    {
+      get;
+      private set;
+    }
+
+
+
+    public TimeSpan ExecutionTime
+    {
+      get;
+      private set;
+    }
+
+    public TimeSpan QueryTime
+    {
+      get;
+      private set;
+    }
+
+    public Exception Exception
+    {
+      get;
+      private set;
+    }
+
+
+    private Action<DbTracing> callback;
+
+
+    void IDbTracing.OnExecuting( object commandObject )
+    {
+      executionStopwatch.Restart();
+      queryStopwatch.Restart();
+    }
+
+    void IDbTracing.OnLoadingData( IDbExecuteContext context )
+    {
+      executionStopwatch.Stop();
+      ExecutionTime = executionStopwatch.Elapsed;
+    }
+
+    void IDbTracing.OnComplete()
+    {
+      queryStopwatch.Stop();
+      QueryTime = queryStopwatch.Elapsed;
+
+      if ( callback != null )
+        callback( this );
+    }
+
+    void IDbTracing.OnException( Exception exception )
+    {
+      executionStopwatch.Stop();
+      queryStopwatch.Stop();
+
+      QueryTime = queryStopwatch.Elapsed;
+
+      Exception = exception;
+
+      if ( callback != null )
+        callback( this );
+    }
+  }
+}
