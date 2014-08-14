f5ae12433f76e812db598bc5d04a3658da7cafd6
Ivony <Ivony@live.com>
2014-08-14 18:31:11 +0800

    EntityExtensions.cs 文件拆分为两个

--- Sources/Ivony.Data/EntityExtensions.cs
+++ Sources/Ivony.Data/EntityExtensions.cs
@@ -318,18 +318,18 @@ namespace Ivony.Data
       }
 
 
-      var convertType = converter ?? EntityConverterCache<T>.GetConverter();
+      var entityConverter = converter ?? EntityConverterCache<T>.GetConverter();
 
 
       var entity = new T();
 
-      if ( convertType.NeedPreconversion )
+      if ( entityConverter.NeedPreconversion )
       {
         var method = CreateEntityConvertMethod<T>();
         method( dataItem, entity );
       }
 
-      convertType.Convert( dataItem, entity );
+      entityConverter.Convert( dataItem, entity );
       return entity;
     }
 
@@ -528,6 +528,8 @@ namespace Ivony.Data
     {
       private static IEntityConverter<T> converterInstacne;
 
+      private static Func<IEntityConverter<T>> createConverter;
+
       public static IEntityConverter<T> GetConverter()
       {
         var instance = converterInstacne;
@@ -553,96 +555,6 @@ namespace Ivony.Data
         return instance;
       }
     }
-  }
-
-  /// <summary>
-  /// 用于指定字段名称的特性
-  /// </summary>
-  [AttributeUsage( AttributeTargets.Property, Inherited = false )]
-  public class FieldNameAttribute : Attribute
-  {
-    /// <summary>
-    /// 创建 FieldNameAttribute 对象
-    /// </summary>
-    /// <param name="name">字段名</param>
-    public FieldNameAttribute( string name )
-    {
-      FieldName = name;
-    }
-
-    /// <summary>
-    /// 字段名
-    /// </summary>
-    public string FieldName
-    {
-      get;
-      set;
-    }
-  }
-
-
-  /// <summary>
-  /// 用于指示属性与任何字段没有关系
-  /// </summary>
-  [AttributeUsage( AttributeTargets.Property, Inherited = false )]
-  public class NonFieldAttribute : Attribute
-  {
-  }
-
-
-  /// <summary>
-  /// 指定类型所应使用的实体转换器
-  /// </summary>
-  [AttributeUsage( AttributeTargets.Class, Inherited = false )]
-  public class EntityConvertAttribute : Attribute
-  {
-
-    private Type _convertType;
-
-    /// <summary>
-    /// 创建 EntityConvertAttribute 对象
-    /// </summary>
-    /// <param name="convertType">实体转换器类型</param>
-    public EntityConvertAttribute( Type convertType )
-    {
-      _convertType = convertType;
-    }
-
-    /// <summary>
-    /// 创建实体转换器实例
-    /// </summary>
-    /// <typeparam name="T">实体类型</typeparam>
-    /// <returns>实体转换器实例</returns>
-    public IEntityConverter<T> CreateConverter<T>()
-    {
-      return (IEntityConverter<T>) Activator.CreateInstance( _convertType );
-    }
-
-  }
-
-  /// <summary>
-  /// 定义实体转换器类型
-  /// </summary>
-  /// <typeparam name="T">实体类型</typeparam>
-  public interface IEntityConverter<T>
-  {
-    /// <summary>
-    /// 将数据写入实体
-    /// </summary>
-    /// <param name="dataItem">数据行</param>
-    /// <param name="entity">要写入数据的实体</param>
-    /// <returns></returns>
-    void Convert( DataRow dataItem, T entity );
-
-    /// <summary>
-    /// 是否可重用
-    /// </summary>
-    bool IsReusable { get; }
-
-    /// <summary>
-    /// 是否需要预转换
-    /// </summary>
-    bool NeedPreconversion { get; }
   }
 
 
