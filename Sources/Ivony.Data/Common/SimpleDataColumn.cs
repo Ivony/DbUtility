6d7944bcbb5af3381ac7f5d4d3ab5d36cd9270ce
Ivony <Ivony@live.com>
2014-09-25 15:31:44 +0800

    新增 SimpleDataTable 体系

---
+++ Sources/Ivony.Data/Common/SimpleDataColumn.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ivony.Data.Common
+{
+  public class SimpleDataColumn
+  {
+
+    private SimpleDataColumn()
+    {
+
+    }
+
+
+
+
+    private class SimpleDataColumn<T> : SimpleDataColumn
+    {
+
+      private List<T> dataList = new List<T>();
+
+
+      public void AddDataItem( T data )
+      {
+        dataList.Add( data );
+      }
+    }
+
+    internal static SimpleDataColumn CreateDataColumn( string name, Type fieldType )
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
--- Sources/Ivony.Data/Common/SimpleDataTable.cs
+++ Sources/Ivony.Data/Common/SimpleDataTable.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,42 @@ namespace Ivony.Data.Common
   /// <summary>
   /// DataTable 的功能缩减版本
   /// </summary>
-  public class SimpleDataTable
+  public sealed class SimpleDataTable
   {
+
+
+
+    private bool _initialized = false;
+
+    private void Initialize( IDataRecord record )
+    {
+
+      Columns = new SimpleDataColumn[record.FieldCount];
+
+      for ( int i = 0; i < Columns.Length; i++ )
+      {
+        var fieldType = record.GetFieldType( i );
+        var name = record.GetName( i );
+        Columns[i] = SimpleDataColumn.CreateDataColumn( name, fieldType );
+
+      }
+
+      _initialized = true;
+    }
+
+    private void Fill( IDataRecord record )
+    {
+      if ( Columns == null )
+        Initialize( record );
+
+    }
+
+
+    public SimpleDataColumn[] Columns
+    {
+      get;
+      private set;
+    }
+
   }
 }
