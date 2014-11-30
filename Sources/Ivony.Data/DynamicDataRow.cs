c10970e8fbf1530b6f1e1069110c7463e1a50087
Ivony <Ivony@live.com>
2014-11-30 23:43:28 +0800

    引入 DynamicDataValue 对象

--- Sources/Ivony.Data/DynamicDataRow.cs
+++ Sources/Ivony.Data/DynamicDataRow.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Dynamic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,7 +21,19 @@ namespace Ivony.Data
       DataRow = dataRow;
     }
 
-    public DynamicMetaObject GetMetaObject( Expression parameter )
+
+
+    public DynamicDataValue GetValue( string name, bool ignoreCase )
+    {
+      var column = DataRow.Table.Columns[name];
+      if ( column == null )
+        return null;
+
+      return new DynamicDataValue( DataRow, column );
+    }
+
+
+    DynamicMetaObject IDynamicMetaObjectProvider.GetMetaObject( Expression parameter )
     {
       return new DataRowMetaObject( this, parameter );
     }
@@ -32,28 +45,48 @@ namespace Ivony.Data
       public Expression Expression { get; private set; }
 
       public DataRowMetaObject( DynamicDataRow row, Expression expression )
-        : base( expression, BindingRestrictions.Empty )
+        : base( expression, BindingRestrictions.Empty, row )
       {
         DataRow = row;
         Expression = expression;
       }
 
 
+
+
+
+      private static readonly MethodInfo GetValueMethod = typeof( DynamicDataRow ).GetMethod( "GetValue" );
+
+
+
       public override DynamicMetaObject BindGetMember( GetMemberBinder binder )
       {
-        return base.BindGetMember( binder );
+
+        var expression = Expression;
+        expression = Expression.Convert( expression, typeof( DynamicDataRow ) );
+        expression = Expression.Call( expression, GetValueMethod, Expression.Constant( binder.Name, typeof( string ) ), Expression.Constant( binder.IgnoreCase, typeof( bool ) ) );
+
+        return new DynamicMetaObject( expression, BindingRestrictions.GetTypeRestriction( Expression, typeof( DynamicDataRow ) ) );
       }
 
       public override DynamicMetaObject BindGetIndex( GetIndexBinder binder, DynamicMetaObject[] indexes )
       {
-        return base.BindGetIndex( binder, indexes );
+
+        if ( indexes.Length != 1 || indexes[0].LimitType != typeof( string ) )
+          return null;
+
+        var name = indexes[0];
+        return new DynamicMetaObject( Expression.Call( Expression, GetValueMethod, name.Expression ), BindingRestrictions.Empty );
       }
 
       public override DynamicMetaObject BindConvert( ConvertBinder binder )
       {
 
         if ( binder.ReturnType.IsAssignableFrom( typeof( DataRow ) ) )
-          return new DynamicMetaObject( Expression, BindingRestrictions.GetTypeRestriction( Expression, typeof( DataRow ) ) );
+        {
+          var expression = Expression.Property( Expression, "DataRow" );
+          return new DynamicMetaObject( expression, BindingRestrictions.Empty );
+        }
 
         return base.BindConvert( binder );
       }
---
+++ Sources/Ivony.Data/DynamicDataValue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Dynamic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Ivony.Data
+{
+  public class DynamicDataValue : IDynamicMetaObjectProvider
+  {
+    public DataRow DataRow { get; private set; }
+    public DataColumn DataColumn { get; private set; }
+    public object DataValue { get; private set; }
+
+
+
+    internal DynamicDataValue( DataRow row, DataColumn column )
+    {
+      DataRow = row;
+      DataColumn = column;
+
+      DataValue = DataRow[DataColumn];
+    }
+
+
+    DynamicMetaObject IDynamicMetaObjectProvider.GetMetaObject( Expression parameter )
+    {
+      return new DynamicValueMetaObject( this, Expression.Convert( parameter, typeof( DynamicDataValue ) ) );
+    }
+
+    private class DynamicValueMetaObject : DynamicMetaObject
+    {
+
+      public DynamicValueMetaObject( DynamicDataValue value, Expression expression ) : base( expression, BindingRestrictions.Empty, value ) { }
+
+
+      public override DynamicMetaObject BindConvert( ConvertBinder binder )
+      {
+        var type = binder.Type;
+        throw new NotImplementedException();
+      }
+
+      public override DynamicMetaObject BindBinaryOperation( BinaryOperationBinder binder, DynamicMetaObject arg )
+      {
+
+        var expression = Expression;
+        expression = Expression.Property( expression, "DataValue" );
+        expression = Expression.MakeBinary( binder.Operation, expression, arg.Expression );
+
+        return new DynamicMetaObject( expression, BindingRestrictions.Empty );
+      }
+    }
+  }
+}
