67c0c6f903b7f7708aebbe9a7bda49ec05b3401a
KevinQiangK <KevinQiang.K@gmail.com>
2015-03-17 13:34:15 +0800

    增加了对Access数据库的支持；
    -在创建Access数据库的时候使用了拷贝空Access数据库的方法；
    -测试用例使用了SQLiteTest的用例；

---
+++ Sources/Ivony.Data.Access/AccessClient/AccessDbConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ivony.Data;
+
+namespace Ivony.Data.Access.AccessClient
+{
+    public class AccessDbConfiguration:DbConfiguration
+    {
+        public AccessDbConfiguration()
+        {
+        }
+    }
+}
---
+++ Sources/Ivony.Data.Access/AccessClient/AccessDbExecutor.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using Ivony.Data.Common;
+using Ivony.Data.Queries;
+
+namespace Ivony.Data.Access.AccessClient
+{
+    public class AccessDbExecutor : DbExecutorBase, IDbExecutor<ParameterizedQuery>
+    {
+
+        public AccessDbExecutor(string connectionString, AccessDbConfiguration configuration)
+            : base(configuration)
+        {
+            if(string.IsNullOrEmpty(connectionString))
+                throw new ArgumentNullException("connectionString");
+            if(configuration == null)
+                throw new ArgumentNullException("configuration");
+
+            Configuration = configuration;
+            Connection = new OleDbConnection(connectionString);
+        }
+
+
+        public OleDbConnection Connection { get; private set; }
+
+
+        protected AccessDbConfiguration Configuration { get; private set; }
+
+        public object SyncRoot { get; private set; }
+
+        public IDbExecuteContext Execute(ParameterizedQuery query)
+        {
+            var tracing = TryCreateTracing(this, query);
+            var command = new AccessDbParameterizedQueryParser().Parse(query);
+            return Execue(command, tracing);
+        }
+
+        private IDbExecuteContext Execue(OleDbCommand command, IDbTracing tracing)
+        {
+            try
+            {
+                TryExecuteTracing(tracing, t => t.OnExecuting(command));
+
+                if (Connection.State == ConnectionState.Closed)
+                    Connection.Open();
+                command.Connection = Connection;
+
+                if (Configuration.QueryExecutingTimeout.HasValue)
+                    command.CommandTimeout = (int)Configuration.QueryExecutingTimeout.Value.TotalSeconds;
+
+                var context = new AccessDbExecuteContext(command.ExecuteReader(), tracing, SyncRoot);
+
+                TryExecuteTracing(tracing, t => t.OnLoadingData(context));
+                return context;
+
+            }
+            catch (DbException exception)
+            {
+                TryExecuteTracing(tracing, t => t.OnException(exception));
+                throw;
+            }
+        }
+
+
+
+    }
+}
